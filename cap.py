3f601defdb323253c5ec05eb5b5b88729036ae05
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2022-06-02 12:13:18 +0200

    test

--- cap.py
+++ cap.py
@@ -2,15 +2,18 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(17, 10000)
+if not cap.isOpened():
+    print('Could not find device.')
+    exit()
+# cap.set(17, 10000)
 counter = 0
 while(True):
     ret, frame = cap.read()
     # frame = cv2.blur(frame,(7,7),0)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break;
+        break
     elif cv2.waitKey(1) & 0xFF == ord('s'):
         cv2.imwrite(f'{counter}.png', frame)
         counter += 1
