24334ddd2a067ae6531dae9484757b8c466091ee
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2022-06-02 14:19:33 +0200

    set / get settings

--- cap.py
+++ cap.py
@@ -7,7 +7,7 @@ if not cap.isOpened():
     exit()
 # cap.set(17, 10000)
 counter = 0
-name ='1Euro'
+name ='test'
 while(True):
     ret, frame = cap.read()
     # frame = cv2.blur(frame,(7,7),0)
@@ -17,6 +17,24 @@ while(True):
         break
     elif cv2.waitKey(1) & 0xFF == ord('s'):
         cv2.imwrite(f'{name}{counter}.png', frame)
+        print('Brightness:' + str(cap.get(10)))
+        print('Contrast:' + str(cap.get(11)))
+        print('Gain:' + str(cap.get(14)))
+        print('Exposure:' + str(cap.get(15)))
+        print('White balance:' + str(cap.get(17)))
         counter += 1
+    elif cv2.waitKey(1) & 0xFF == ord('n'):
+        name = input('Name:\n')
+        counter = 0
+    elif cv2.waitKey(1) & 0xFF == ord('b'):
+        cap.set(10, int(input('Brightness (0 - 255):\n')))
+    elif cv2.waitKey(1) & 0xFF == ord('c'):
+        cap.set(11, int(input('Contrast (0 - 255):\n')))
+    elif cv2.waitKey(1) & 0xFF == ord('g'):
+        cap.set(14, int(input('Gain (0 - 255):\n')))
+    elif cv2.waitKey(1) & 0xFF == ord('e'):
+        cap.set(15, int(input('Exposure (-1 to -7):\n')))
+    elif cv2.waitKey(1) & 0xFF == ord('w'):
+        cap.set(17, int(input('Whitebalance (-1 to 10000):\n')))
 cap.release()
 cv2.destroyAllWindows()
