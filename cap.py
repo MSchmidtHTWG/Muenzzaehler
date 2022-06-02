d29226a7f892eb3378da00c01b80b0225852449b
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2022-06-02 13:06:03 +0200

    images

--- cap.py
+++ cap.py
@@ -7,6 +7,7 @@ if not cap.isOpened():
     exit()
 # cap.set(17, 10000)
 counter = 0
+name ='1Euro'
 while(True):
     ret, frame = cap.read()
     # frame = cv2.blur(frame,(7,7),0)
@@ -15,7 +16,7 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     elif cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite(f'{counter}.png', frame)
+        cv2.imwrite(f'{name}{counter}.png', frame)
         counter += 1
 cap.release()
 cv2.destroyAllWindows()
