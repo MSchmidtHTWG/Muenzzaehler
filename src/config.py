b6ba2056f5ee7684c2009058b6645ad0f685e6af
nstre <n.strenger1@gmail.com>
2022-06-04 12:59:17 +0200

    current

---
+++ src/config.py
@@ -0,0 +1,24 @@
+from typing import Dict, Any
+
+config: dict[Any, Any] = {
+    "camera-settings": {
+        "width": 640,
+        "brightness": 0,
+        "contrast": 0,
+        "saturation": 0,
+        "gain": 0,
+        "exposure": 0,
+        "white balance": 1,
+    },
+    "coins": {
+        "1cent": {
+            "min": 400,
+            "max": 500,
+            "color":,
+        },
+        "2cent": {
+            "min":,
+        },
+
+    }
+}
