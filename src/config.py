2732fab520f9149ca1d50bdf057a96fea0c71961
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2022-06-05 10:05:26 +0200

    test

--- src/config.py
+++ src/config.py
@@ -2,13 +2,24 @@ from typing import Dict, Any
 
 config: dict[Any, Any] = {
     "camera-settings": {
-        "width": 640,
-        "brightness": 0,
-        "contrast": 0,
-        "saturation": 0,
-        "gain": 0,
-        "exposure": 0,
-        "white balance": 1,
+        "highcontrast": {
+            "width": 640,
+            "brightness": 180,
+            "contrast": 255,
+            "saturation": 0,
+            "gain": 32,
+            "exposure": -2,
+            "white balance": 1,
+        },
+        "lowcontrast": {
+            "width": 640,
+            "brightness": 0,
+            "contrast": 0,
+            "saturation": 0,
+            "gain": 0,
+            "exposure": 0,
+            "white balance": 1,
+        },
     },
     "coins": {
         "1cent": {
